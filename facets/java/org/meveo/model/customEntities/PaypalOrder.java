dc26d2b23f94ee6b6b3ae260130e9bffe864af67
meveo.admin <no-mail@meveo.com>
2022-01-26 04:18:52 +0000

    Add the cet java source file : PaypalOrder.javain the module : LIQUICHAIN_PAYMENT

--- facets/java/org/meveo/model/customEntities/PaypalOrder.java
+++ facets/java/org/meveo/model/customEntities/PaypalOrder.java
@@ -3,6 +3,8 @@ package org.meveo.model.customEntities;
 import org.meveo.model.CustomEntity;
 import java.util.List;
 import org.meveo.model.persistence.DBStorageType;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import java.time.Instant;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
 public class PaypalOrder implements CustomEntity {
@@ -19,6 +21,33 @@ public class PaypalOrder implements CustomEntity {
     @JsonIgnore()
     private DBStorageType storages;
 
+    private String orderId;
+
+    @JsonProperty(required = true)
+    private String toCurrency;
+
+    @JsonProperty(required = true)
+    private String fromWallet;
+
+    @JsonProperty(required = true)
+    private String fromAmount;
+
+    @JsonProperty(required = true)
+    private String toAmount;
+
+    @JsonProperty(required = true)
+    private String fromCurrency;
+
+    @JsonProperty(required = true)
+    private Instant creationDate;
+
+    private String error;
+
+    private String status;
+
+    @JsonProperty(required = true)
+    private String toWallet;
+
     @Override()
     public String getUuid() {
         return uuid;
@@ -34,6 +63,86 @@ public class PaypalOrder implements CustomEntity {
 
     public void setStorages(DBStorageType storages) {
         this.storages = storages;
+    }
+
+    public String getOrderId() {
+        return orderId;
+    }
+
+    public void setOrderId(String orderId) {
+        this.orderId = orderId;
+    }
+
+    public String getToCurrency() {
+        return toCurrency;
+    }
+
+    public void setToCurrency(String toCurrency) {
+        this.toCurrency = toCurrency;
+    }
+
+    public String getFromWallet() {
+        return fromWallet;
+    }
+
+    public void setFromWallet(String fromWallet) {
+        this.fromWallet = fromWallet;
+    }
+
+    public String getFromAmount() {
+        return fromAmount;
+    }
+
+    public void setFromAmount(String fromAmount) {
+        this.fromAmount = fromAmount;
+    }
+
+    public String getToAmount() {
+        return toAmount;
+    }
+
+    public void setToAmount(String toAmount) {
+        this.toAmount = toAmount;
+    }
+
+    public String getFromCurrency() {
+        return fromCurrency;
+    }
+
+    public void setFromCurrency(String fromCurrency) {
+        this.fromCurrency = fromCurrency;
+    }
+
+    public Instant getCreationDate() {
+        return creationDate;
+    }
+
+    public void setCreationDate(Instant creationDate) {
+        this.creationDate = creationDate;
+    }
+
+    public String getError() {
+        return error;
+    }
+
+    public void setError(String error) {
+        this.error = error;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getToWallet() {
+        return toWallet;
+    }
+
+    public void setToWallet(String toWallet) {
+        this.toWallet = toWallet;
     }
 
     @Override()
