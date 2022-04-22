9de7324d8a7690ddfc29ab46b3efa2337867c66a
tonysviews <tonysviews@gmail.com>
2022-04-22 03:28:09 +0000

    Add JS script for Endpoint: currconv

--- facets/javascript/endpoints/currconv.js
+++ facets/javascript/endpoints/currconv.js
@@ -1,53 +1,28 @@
-import EndpointInterface from "#{API_BASE_URL}/api/rest/endpoint/EndpointInterface.js";
-
-// the request schema, this should be updated
-// whenever changes to the endpoint parameters are made
-// this is important because this is used to validate and parse the request parameters
-const requestSchema = {
-  "title" : "currconvRequest",
-  "id" : "currconvRequest",
-  "default" : "Schema definition for currconv",
-  "$schema" : "http://json-schema.org/draft-07/schema",
-  "type" : "object"
+const currconv = async (parameters) =>  {
+	const baseUrl = window.location.origin;
+	const url = new URL(`${window.location.pathname.split('/')[1]}/rest/currconv/`, baseUrl);
+	return fetch(url.toString(), {
+		method: 'GET'
+	});
 }
 
-// the response schema, this should be updated
-// whenever changes to the endpoint parameters are made
-// this is important because this could be used to parse the result
-const responseSchema = {
-  "title" : "currconvResponse",
-  "id" : "currconvResponse",
-  "default" : "Schema definition for currconv",
-  "$schema" : "http://json-schema.org/draft-07/schema",
-  "type" : "object",
-  "properties" : {
-    "result" : {
-      "title" : "result",
-      "type" : "string",
-      "minLength" : 1
-    }
-  }
+const currconvForm = (container) => {
+	const html = `<form id='currconv-form'>
+		<button type='button'>Test</button>
+	</form>`;
+
+	container.insertAdjacentHTML('beforeend', html)
+
+
+	container.querySelector('#currconv-form button').onclick = () => {
+		const params = {
+
+		};
+
+		currconv(params).then(r => r.text().then(
+				t => alert(t)
+			));
+	};
 }
 
-// should contain offline mock data, make sure it adheres to the response schema
-const mockResult = {};
-
-class currconv extends EndpointInterface {
-	constructor() {
-		// name and http method, these are inserted when code is generated
-		super("currconv", "GET");
-		this.requestSchema = requestSchema;
-		this.responseSchema = responseSchema;
-		this.mockResult = mockResult;
-	}
-
-	getRequestSchema() {
-		return this.requestSchema;
-	}
-
-	getResponseSchema() {
-		return this.responseSchema;
-	}
-}
-
-export default new currconv();
+export { currconv, currconvForm };
