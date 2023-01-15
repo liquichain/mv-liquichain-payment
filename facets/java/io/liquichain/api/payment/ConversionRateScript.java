97f251e7d4443e14ee8fc9c612aab7af5b923404
Tony Alejandro <tonysviews@gmail.com>
2023-01-15 08:47:06 +0800

    update ConversionRateScript

--- facets/java/io/liquichain/api/payment/ConversionRateScript.java
+++ facets/java/io/liquichain/api/payment/ConversionRateScript.java
@@ -17,15 +17,12 @@ public class ConversionRateScript extends Script {
         return result;
     }
 
-    public static BigDecimal ONE = new BigDecimal(1);
     public static BigDecimal LCN_TO_EUR = new BigDecimal("2000").setScale(9, HALF_UP);
-    public static BigDecimal EUR_TO_LCN = ONE.divide(LCN_TO_EUR, 9, HALF_UP);
+    public static BigDecimal EUR_TO_LCN = BigDecimal.ONE.divide(LCN_TO_EUR, 9, HALF_UP);
     public static BigDecimal KLUB_TO_EUR = new BigDecimal("0.014").setScale(9, HALF_UP);
-    public static BigDecimal EUR_TO_KLUB = ONE.divide(KLUB_TO_EUR, 9, HALF_UP);
-    public static BigDecimal CFA_TO_EUR = new BigDecimal("0.0015").setScale(9, HALF_UP);
-    public static BigDecimal EUR_TO_CFA = new BigDecimal("655.13").setScale(9, HALF_UP);
+    public static BigDecimal EUR_TO_KLUB = BigDecimal.ONE.divide(KLUB_TO_EUR, 9, HALF_UP);
     public static BigDecimal KLUB_TO_USD = new BigDecimal("0.015").setScale(9, HALF_UP);
-    public static BigDecimal USD_TO_KLUB = ONE.divide(KLUB_TO_USD, 9, HALF_UP);
+    public static BigDecimal USD_TO_KLUB = BigDecimal.ONE.divide(KLUB_TO_USD, 9, HALF_UP);
 
     public static final Map<String, BigDecimal> CONVERSION_RATE = new HashMap<>();
 
@@ -34,8 +31,6 @@ public class ConversionRateScript extends Script {
         CONVERSION_RATE.put("EUR_TO_LCN", EUR_TO_LCN);
         CONVERSION_RATE.put("KLUB_TO_EUR", KLUB_TO_EUR);
         CONVERSION_RATE.put("EUR_TO_KLUB", EUR_TO_KLUB);
-        CONVERSION_RATE.put("EUR_TO_CFA", EUR_TO_CFA);
-        CONVERSION_RATE.put("CFA_TO_EUR", CFA_TO_EUR);
         CONVERSION_RATE.put("KLUB_TO_USD", KLUB_TO_USD);
         CONVERSION_RATE.put("USD_TO_KLUB", USD_TO_KLUB);
     }
@@ -48,8 +43,6 @@ public class ConversionRateScript extends Script {
             + "{\"from\":{\"value\":1,\"currency\":\"EUR\"},\"to\":{\"value\":" + EUR_TO_LCN + ",\"currency\":\"LCN\"}},\n"
             + "{\"from\":{\"value\":1,\"currency\":\"KLUB\"},\"to\":{\"value\":" + KLUB_TO_EUR + ",\"currency\":\"EUR\"}},\n"
             + "{\"from\":{\"value\":1,\"currency\":\"EUR\"},\"to\":{\"value\":" + EUR_TO_KLUB + ",\"currency\":\"KLUB\"}},\n"
-            + "{\"from\":{\"value\":1,\"currency\":\"EUR\"},\"to\":{\"value\":" + EUR_TO_CFA + ",\"currency\":\"CFA\"}},\n"
-            + "{\"from\":{\"value\":1,\"currency\":\"CFA\"},\"to\":{\"value\":" + CFA_TO_EUR + ",\"currency\":\"EUR\"}},\n"
             + "{\"from\":{\"value\":1,\"currency\":\"KLUB\"},\"to\":{\"value\":" + KLUB_TO_USD + ",\"currency\":\"USD\"}},\n"
             + "{\"from\":{\"value\":1,\"currency\":\"USD\"},\"to\":{\"value\":" + USD_TO_KLUB + ",\"currency\":\"KLUB\"}}\n"
             + "],\n"
